7de94737fa40c1a724687b4977da9a0e953f18de
Gourav Suri <gourav96suri@gmail.com>
2016-11-13 20:37:06 +0000

    added index page of companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,5 +5,5 @@ class Company < ActiveRecord::Base
     validates :location, presence: true, length: { minimum: 10, maximum: 300 }
     validates :package, presence: true
     validates :description, presence: true, length: {minimum: 10, maximum: 1000 }
-    validates_format_of :date, presence: true, :with => /\d{2}\/\d{2}\/\d{4}/, :message => "^Date must be in the following format: mm/dd/yyyy"
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get '/home', to: 'pages#home'
+  
+  resources :companies
 end
