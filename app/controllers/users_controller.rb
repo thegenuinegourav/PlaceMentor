8b34371584671fc12cfe6951ab0c2c27907751d4
Gourav Suri <gourav96suri@gmail.com>
2016-11-19 14:11:39 +0000

    Fixed create new user Bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     
     before_action :set_user, only: [:edit, :update, :show]
     before_action :require_same_user, only: [:edit, :update]
-    before_action :require_user, except: [:new]
+    before_action :require_user, only: [:show]
    
    def new
        @user = User.new
